e7a7c2c207c617381fd7d8cc8c20c7a3fba1b89d
robiel.tesfazghi@cpnv.ch <robiel.tesfazghi@cpnv.ch>
2022-09-01 11:12:58 +0200

    :art: "Improve package folder structure"

---
+++ htdocs/README.md
@@ -0,0 +1,1 @@
+# go_fvaj_faq
---
+++ htdocs/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    './**/*.{html,js.php}',
+    './node_modules/tw-elements/dist/js/**/*.js'
+  ],
+  plugins: [
+    require('tw-elements/dist/plugin')],
+};
