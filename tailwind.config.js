5258afab93f2e332e0b09fafebf42b2a549a5452
robiel.tesfazghi@cpnv.ch <robiel.tesfazghi@cpnv.ch>
2022-09-05 15:59:34 +0200

    :truck: remove old file from git

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: [
-    './htdocs/**/*.{html,js.php}',
-    './node_modules/tw-elements/dist/js/**/*.js'
-  ],
-  plugins: [
-    require('tw-elements/dist/plugin')],
-};
