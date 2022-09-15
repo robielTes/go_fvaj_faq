5fe219c8df8f6a5fe585698f53a4e032da04f9bc
robiel.tesfazghi@cpnv.ch <robiel.tesfazghi@cpnv.ch>
2022-09-15 11:41:12 +0200

    :sparkles: Update admin.php to generate html page

--- htdocs/admin.php
+++ htdocs/admin.php
@@ -49,17 +49,28 @@ if(isset($_POST["submit"])){
 }
 
 if(isset($_POST["save"])){
-  echo "<pre>";
-  print_r($_POST);
-  echo "</pre>";
+  display_main();
+  generate_html_page();
+  unset($_POST["save"]);
 }
+
+function generate_html_page(){
+  $file = "$_SESSION[type].html";
+  // Open the file to get existing content
+  $current .= file_get_contents("./templates/header.html");
+  $current .= display_main();
+  $current .= file_get_contents("./templates/footer.html");
+  // Write the contents back to the file
+  file_put_contents($file,$current);
+}
+
 
 function display_faq(){
   $faq_data ="";
   
   foreach(get_information()["Information"] as $key => $value){
     $key++;
-    $faq_data.= '<button class="w-full bg-white text-gray-800 rounded-lg hover:bg-slate-100 hover:text-gray-600 active:hover:text-gray-400 flex pb-3 pt-4 px-5 title-font font-medium border border-black-100" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'.$key.' aria-expanded="false" aria-controls="collapseExample">';
+    $faq_data.= '<button class="w-full bg-white text-gray-800 rounded-lg hover:bg-slate-100 hover:text-gray-600 active:hover:text-gray-400 flex pb-3 pt-4 px-5 title-font font-medium border border-black-100" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'.$key.' "aria-expanded="false" aria-controls="collapseExample">';
     $faq_data.=    "#{$key} {$value["title"]}";      
     $faq_data.= '</button>';
     $faq_data.=  '<div class="collapse" id="collapse'.$key.'">';
@@ -70,6 +81,26 @@ function display_faq(){
   }
   return $faq_data;
 }
+
+function display_main(){
+  $main_data = "";
+  $main_data .= '<main style="background-image: url(\'./img/banner.jpg\');" class="bg-cover border border-white">';
+  $main_data .= '<div class="container pb-16">';
+  $main_data .=     '<div class="d-flex flex-column bd-highlight m-16 ">';
+  $main_data .=       '<div style=" background-image: linear-gradient(rgba(0, 255, 0, 0.5), rgba(255, 255, 255, 0.5)); height:auto; min-height: 10em; padding:3px; margin:3px;">';
+  $main_data .=         '<h1 class="title display-4 text-center font-normal text-5xl p-4">';
+  $main_data .=             get_information()["title"];
+  $main_data .=            '<a href="./index.html" class="hover:text-gray-400 text-white border-b-4 hover:border-gray-400 border-white -mb-6">&#x1F3E0;&#x2303;</a>';
+  $main_data .=         '</h1>';
+  $main_data .=         display_faq();
+  $main_data .=       '</div>';
+  $main_data .=     '</div>';
+  $main_data .=   '</div>';
+  $main_data .= '</main>';
+
+  return $main_data;
+}
+
 
 // check if user is logged in if not show login form
 if(!isset($_SESSION["user_name"])){ 
@@ -143,38 +174,45 @@ if(!isset($_SESSION["user_name"])){
 <?php
 }
 //if user is logged in show admin page
-else{
-?>
-
-<main style="background-image: url('./img/banner.jpg');" class="bg-cover border border-white">
-    <div class="container pb-16">
-      <div class="d-flex flex-column bd-highlight m-16 ">
-          <div class="" style=" background-image: linear-gradient(rgba(0, 255, 0, 0.5), rgba(255, 255, 255, 0.5)); height:auto; min-height: 10em; padding:3px; margin:3px;">
-            <h1 class="title display-4 text-center font-normal text-5xl p-4"><?=get_information()["title"]?>
-              <p class="text-3xl float-left"><?= isset($_SESSION["user_name"])? $_SESSION["user_name"]:"go.fvja.ch"?></p> 
-              <a href="./index.html" class="hover:text-gray-400 text-white border-b-4 hover:border-gray-400 border-white -mb-6">&#x1F3E0;&#x2303;</a>
-              <form action="<?= $_SERVER["PHP_SELF"];?>" method="post" class="p-3 inline">
-              <button type="submit" name="logout" class="inline-flex hover:text-white text-black mx-4 bg-blue-500 bg-black-500  py-1 px-6 focus:outline-none hover:bg-green-600 rounded text-lg mb-6 float-right">Logout</button>
-              </form>
-            </h1>
-              <?=display_faq()?>
-
-            <form action="<?= $_SERVER["PHP_SELF"];?>" method="post" class="flex space-x-2 justify-center">
-              <button type="submit" name="save" class="inline-block px-6 py-2.5 bg-blue-500 text-white leading-tight uppercase rounded-lg shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out">Save</button>
-            </form>
-
-          </div>
-      </div>
-    </div>
-  </main>
-
- 
+else{  
+  ?>
   
-  <hr class="bg-white m-4">
-   
-<?php
-}
+  <main style="background-image: url('./img/banner.jpg');" class="bg-cover border border-white">
+      <div class="container pb-16">
+        <div class="d-flex flex-column bd-highlight m-16 ">
+            <div class="" style=" background-image: linear-gradient(rgba(0, 255, 0, 0.5), rgba(255, 255, 255, 0.5)); height:auto; min-height: 10em; padding:3px; margin:3px;">
+              <h1 class="title display-4 text-center font-normal text-5xl p-4"><?=get_information()["title"]?>
+                <p class="text-3xl float-left"><?= isset($_SESSION["user_name"])? $_SESSION["user_name"]:"go.fvja.ch"?></p> 
+                <a href="./index.html" class="hover:text-gray-400 text-white border-b-4 hover:border-gray-400 border-white -mb-6">&#x1F3E0;&#x2303;</a>
+                <form action="<?= $_SERVER["PHP_SELF"];?>" method="post" class="p-3 inline">
+                <button type="submit" name="logout" class="inline-flex hover:text-white text-black mx-4 border bg-slate-100 bg-black-500  py-1 px-6 focus:outline-none hover:bg-green-600 rounded text-lg mb-6 float-right">Logout</button>
+                </form>
+              </h1>
+              
+              <?php foreach(get_information()["Information"] as $key => $value):?>
+                <?php $key++;?>
+                <button class="w-full bg-white text-gray-800 rounded-lg hover:bg-slate-100 hover:text-gray-600 active:hover:text-gray-400 flex pb-3 pt-4 px-5 title-font font-medium border border-black-100" 
+                type="button" data-bs-toggle="collapse" data-bs-target="<?="#collapse{$key}"?>" aria-expanded="false" aria-controls="collapseExample">
+                  <?="#{$key} {$value["title"]}"?>
+                </button>
+                <div class="collapse" id="<?="collapse{$key}"?>">
+                  <div class="block p-5 shadow-lg bg-white rounded-lg">
+                    <strong><?=$value["question"]?></strong><?=$value["answer"]?>
+                  </div>
+                </div>
+              <?php endforeach ?>
 
-include_once("./templates/footer.html");
-
-
+              <form action="<?=$_SERVER["PHP_SELF"]?>" method="post" class="flex space-x-2 justify-center">
+                  <button type="submit" name="save" class="inline-block px-6 py-2.5 bg-blue-500 text-white leading-tight uppercase rounded-lg shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out">Save</button>
+              </form>
+  
+            </div>
+        </div>
+      </div>
+    </main>
+     
+  <?php
+  }
+  
+  include_once("./templates/footer.html");
+  
